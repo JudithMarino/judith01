e707bd499890c6007cea56474bc0f47ed2c16235
JudithMarino <judithmarino28@gmail.com>
2025-05-06 21:58:58 -0600

    Update colab.js

--- colab.js
+++ colab.js
@@ -1,13 +1,24 @@
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 15,
-      stretch: 0,
-      depth: 300,
-      modifier: 1,
-    },
-    loop: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 15,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+  },
+  loop: true,
+});
+
+var swiperSlides = document.querySelectorAll('.swiper-slide');
+
+swiperSlides.forEach(function(slide) {
+  slide.addEventListener('click', function() {
+    var link = this.querySelector('a');
+    if (link) {
+      window.location.href = link.href;
+    }
   });
+});
