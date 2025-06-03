3936be71a37133bb8fd277b015b2551c074f3883
JudithMarino <judithmarino28@gmail.com>
2025-06-03 11:39:49 -0600

    Delete dato.js

--- dato.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.summary').forEach((elem) => {
-  elem.addEventListener('click', () => {
-    const content = elem.parentElement.querySelector('.content')
-    const isOpen = elem.parentElement.classList.toggle('open');
-    content.style.height = isOpen ? content.scrollHeight + "px" : 0
-  })
-})
