2f1e9d638c20b3f02aa9b75cbc5324f4dc2ac4fc
JudithMarino <judithmarino28@gmail.com>
2025-06-03 17:52:14 -0600

    Delete datos.js

--- datos.js
+++
@@ -1,8 +0,0 @@
-const headers = document.querySelectorAll('.accordion-header');
-
-headers.forEach(header => {
-  header.addEventListener('click', () => {
-    const body = header.nextElementSibling;
-    body.classList.toggle('show');
-  });
-});
